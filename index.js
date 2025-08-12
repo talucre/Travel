74623bf3238f89c15864e1efafd9913c8947fe9b
talucre <talucreee@gmail.com>
2025-08-13 01:54:26 +0300

    completed

--- index.js
+++ index.js
@@ -74,3 +74,14 @@ ScrollReveal().reveal(".banner__card", {
   ...scrollRevealOption,
   interval: 500,
 });
+
+ScrollReveal().reveal(".discover__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  loop: true,
+});
